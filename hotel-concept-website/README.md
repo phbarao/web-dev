f541c9a6018f5b7854a763758fcdadfa3e05cc90
Pedro Barão <phbarao@gmail.com`>
2020-05-21 23:28:56 -0300

    adicionando projeto hotel-concept-website

---
+++ hotel-concept-website/README.md
@@ -0,0 +1,2 @@
+# web-dev
+Projeto desenvolvido acompanhando um tutorial no youtube. Um layout responsivo, utilizado vários recursos de HTML5 e CSS.
---
+++ hotel-concept-website/main.js
@@ -0,0 +1,7 @@
+//Selectors
+let header = document.querySelector('.header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', function () {
+    header.classList.toggle('menu-open');
+})
